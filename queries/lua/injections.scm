e0a1c3efdc5ec205025ed68458b25ed98e52ea91
Munif Tanjim <hello@muniftanjim.dev>
2021-08-28 19:37:54 +0600

    feat: support injections queries for vim

--- queries/lua/injections.scm
+++ queries/lua/injections.scm
@@ -13,4 +13,14 @@
   arguments: (arguments (string content: _ @c)))
   (#eq? @_cdef_identifier "cdef"))
 
+((function_call
+  name: (dot_index_expression) @_vim_cmd
+  arguments: (arguments (string content: _ @vim)))
+  (#eq? @_vim_cmd "vim.cmd"))
+
+((function_call
+  name: (dot_index_expression) @_vim_api_nvim_exec
+  arguments: (arguments (string content: _ @vim)))
+  (#eq? @_vim_api_nvim_exec "vim.api.nvim_exec"))
+
 (comment) @comment
