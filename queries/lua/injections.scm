a7ff8ea6db29747a10d262f0ab13a0f525ba2cfa
Munif Tanjim <hello@muniftanjim.dev>
2022-01-13 19:08:34 +0600

    feat: improve injections queries for c and vim

--- queries/lua/injections.scm
+++ queries/lua/injections.scm
@@ -1,26 +1,14 @@
 ((function_call
-  name: (identifier) @_cdef_identifier
+  name: [
+    (identifier) @_cdef_identifier
+    (_ _ (identifier) @_cdef_identifier)
+  ]
   arguments: (arguments (string content: _ @c)))
   (#eq? @_cdef_identifier "cdef"))
 
 ((function_call
-  name: (dot_index_expression _ (identifier) @_cdef_identifier)
-  arguments: (arguments (string content: _ @c)))
-  (#eq? @_cdef_identifier "cdef"))
-
-((function_call
-  name: (method_index_expression _ (identifier) @_cdef_identifier)
-  arguments: (arguments (string content: _ @c)))
-  (#eq? @_cdef_identifier "cdef"))
-
-((function_call
-  name: (dot_index_expression) @_vim_cmd
+  name: (_) @_vimcmd_identifier
   arguments: (arguments (string content: _ @vim)))
-  (#eq? @_vim_cmd "vim.cmd"))
-
-((function_call
-  name: (dot_index_expression) @_vim_api_nvim_exec
-  arguments: (arguments (string content: _ @vim)))
-  (#eq? @_vim_api_nvim_exec "vim.api.nvim_exec"))
+  (#any-of? @_vimcmd_identifier "vim.cmd" "vim.api.nvim_command" "vim.api.nvim_exec"))
 
 (comment) @comment
